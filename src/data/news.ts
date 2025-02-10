3029cccf2f8aeef5d212965e2ba3548f0d68911b
Tiankai Xie <42656139+tiankaixie@users.noreply.github.com>
2025-02-09 20:58:16 -0700

    update the news, responsive UI

--- src/data/news.ts
+++ src/data/news.ts
@@ -1,20 +1,35 @@
 export const newsItems = [
   {
-    date: "2025-01-31",
-    title: "Changelog 0.17",
-    link: "#",
-    description: "In development: dashboard, sys-db, LLM copilot",
+    date: '02-09-2025',
+    title: 'Changelog 0.1',
+    link: '#',
+    description: 'New year, new look for the website',
   },
   {
-    date: "2025-01-24",
-    title: "10 unhappy users",
-    link: "#",
-    description: "Scaling Membrane, or anything",
+    date: '01-09-2025',
+    title: 'CG&A Published',
+    link: '#',
+    description:
+      'Paper LossLens: Diagnostics for Machine Learning Through Loss Landscape Visual Analytics is published in Computer Graphics and Applications',
   },
   {
-    date: "2025-01-18",
-    title: "Membrane + Hono API router",
-    link: "#",
-    description: "Pete's onsite hackathon project",
+    date: '11-07-2024',
+    title: 'NeurIPS Workshop (NeurReps) Accepted',
+    link: '#',
+    description:
+      '`Visualizing Loss Functions as Topological Landscape Profiles` has been accepted to NeurIPS 2024 Workshop on Symmetry and Geometry in Neural Representations (NeurReps)',
+  },
+  {
+    date: '11-07-2024',
+    title: 'NeurIPS Workshop (SciForDL) Accepted',
+    link: '#',
+    description:
+      '`Evaluating Loss Landscapes from a Topology Perspective`, has been accepted to NeurIPS 2024 Workshop on Scientific Methods for Understanding Deep Learning (SciForDL).',
+  },
+  {
+    date: '10-12-2024',
+    title: 'New Blog Post',
+    link: '#',
+    description: 'Setting up Remote Server Development Environment',
   },
 ]
